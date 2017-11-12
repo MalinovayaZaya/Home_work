8229a921494e02ed2b4c9a9ddd3722424fba3b63
Polina <32522035+MalinovayaZaya@users.noreply.github.com>
2017-11-12 23:59:02 +0300

    Add files via upload

---
+++ 4/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int N;
+cout << "Enter the number of elements-" << endl;
+cin >> N;
+int A[N];
+for(int i=0;i<N;i++){
+cout << "Enter the element" << i+1 << "-" << endl;
+cin >> A[i];
+}
+double temp[N];
+for (int i=0;i<N;i++){
+        if (A[i]==0)temp[i]=0.5
+        if (A[i]==3||A[i]==2) temp[i]=0;
+         for (int q=2; q*q<=A[i]; q++) {
+          if (A[i]%q==0) temp[i]=1;
+          else temp[i]=0;
+         }
+}
+for (int i=0;i<N;i++){
+if (temp[i]==0) {
+    for (int j=i+1;j<N;j++)
+        if (A[i]==A[j]){
+        A[i]=0;
+        A[j]=0;
+        }
+}
+}
+cout << "Array without repeats prime numbers: " << endl;
+for (int i=0;i<N;i++){
+ if (A[i]!=0) cout << A[i] << endl;
+}
+
+return 0;
+}
