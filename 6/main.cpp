d3502c63d7d6f951d2ba39e040827e946da4565f
Polina <32522035+MalinovayaZaya@users.noreply.github.com>
2017-11-25 00:00:56 +0300

    Add files via upload

---
+++ 6/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+  int main (){
+   int n;
+   cout << "enter the number of rows and columns ";
+   cin >> n;
+   int s=0;
+   int A [n][n];
+    for (int i = 0; i<n; i++){
+    for (int j = i; j<n; j++){
+        A[i][j] = s;
+        s++;
+        }
+        s=0;
+        }
+   int d=n-1;
+   int u=d;
+   for(int i=n-1; i>0; i--){
+    for(int j=0; j<i; j++){
+        A[i][j]=d;
+        d--;
+    }
+    u--;
+    d=u;
+   }
+    cout<<"Array:  "<< endl;
+    for (int i = 0; i < n; i++){
+    cout<<endl;
+        for (int j = 0; j < n; j++){
+            cout << A[i][j] << " ";
+    }
+}
+return 0;
+}
