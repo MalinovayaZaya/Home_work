38b0e8248fe0ce557eba6d7568d46a7eb7e3b35d
Polina <32522035+MalinovayaZaya@users.noreply.github.com>
2017-11-24 23:50:11 +0300

    Update and rename main.cpp to 1_6.cpp

--- 6/1_6.cpp
+++ 6/1_6.cpp
@@ -1,9 +1,10 @@
+//Напишите программу, которая удаляет из массива все простые числа встречающиеся больше одного раза.
 #include <iostream>
 using namespace std;
 void* delete_annoying_primes (int* a,int n){
 int p[n];
-for (int i=0;i<n;i++){
-        if (a[i]==3||a[i]==2) p[i]=0;
+for (int i=0; i<n; i++){
+        if (a[i]==3 || a[i]==2) p[i]=0;
          for (int o=2; o*o<=a[i]; o++) {
           if (a[i]%o==0) {p[i]=1;continue;}
           else p[i]=0;
@@ -11,7 +12,7 @@ for (int i=0;i<n;i++){
 }
 for (int i=0;i<n;i++){
 if (p[i]==0) {
-    for (int j=i+1;j<n;j++)
+    for (int j=i+1; j<n; j++)
         if (a[i]==a[j]){
         a[i]=0;
         a[j]=0;
@@ -24,7 +25,7 @@ int n;
 cout << "How many elements in array? ";
 cin >> n;
 int a[n];
-for(int i=0;i<n;i++){
+for(int i=0; i<n; i++){
 cout << "Enter the element " << i+1 << " ";
 cin >> a[i];
 }
